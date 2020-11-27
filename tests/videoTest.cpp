e0cbe6da21f5b9fd1c65f301a70660b7b81b46b7
Bitate <1660513875@qq.com>
2020-11-27 23:41:49 +0800

    Stuck with linking of Opencv

--- tests/videoTest.cpp
+++ tests/videoTest.cpp
@@ -6,16 +6,16 @@ TEST(videoTest, clipVideoTest)
 {
     Video video;
 
-    std::string targetVideoPath = "...";
-    std::string newVideoPath = "...";
+    std::string targetVideoPath = "C:/Users/16605/Music/Something Just Like This.mp4";
+    std::string newVideoPath = "C:/Users/16605/Music/Something Just Like This Clipped.mp4";
     int beginMilliseconds = 1000;
     int endMilliseconds = 5000;
 
     video.clipVideo(
         targetVideoPath,
-        newVideoPath,
         beginMilliseconds,
-        endMilliseconds
+        endMilliseconds,
+        newVideoPath
     );
     
 }
